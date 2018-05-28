ce9e63f576b815a23f09de106b766bd9f17c206e
Ruud van Engelenhoven <ruud.vanengelenhoven@gmail.com>
2018-05-28 06:25:10 +0400

    added admin theme package

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,9 @@ let mix = require('laravel-mix');
  */
 
 mix.js('resources/assets/js/app.js', 'public/js')
-   .sass('resources/assets/sass/app.scss', 'public/css');
+    .copy('resources/assets/js/admin/require.min.js', 'public/js/require.min.js')
+    .copy('resources/assets/js/admin/dashboard.js', 'public/js/dashboard.js')
+    .copy('resources/assets/js/admin/core.js', 'public/js/core.js')
+    .copyDirectory('resources/assets/js/admin/vendors', 'public/js/vendors')
+    .sass('resources/assets/sass/app.scss', 'public/css')
+    .sass('resources/assets/sass/admin.scss', 'public/css');
